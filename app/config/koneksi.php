228f216607853fc8d491ccf36010b4ac4958d80d
gemahandika <gemahandika88@gmail.com>
2024-03-16 13:10:06 +0700

    perubahan pada file config

--- app/config/koneksi.php
+++ app/config/koneksi.php
@@ -1,1 +1,11 @@
 
+<?php
+// setting default timezone
+date_default_timezone_set('Asia/Jakarta');
+
+
+// koneksi database
+$koneksi = mysqli_connect('localhost', 'u568977811_atk', 'Piss1994@', 'u568977811_db_atk');
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+}
