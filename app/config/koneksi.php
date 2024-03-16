27b2e7e1089bd972d12f72cc73a9a334a5bbba08
gemahandika <gemahandika88@gmail.com>
2024-03-16 13:12:14 +0700

    perubahan file gitignore dan koneksi

--- app/config/koneksi.php
+++ app/config/koneksi.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Jakarta');
 
 
 // koneksi database
-$koneksi = mysqli_connect('localhost', 'u568977811_atk', 'Piss1994@', 'u568977811_db_atk');
+$koneksi = mysqli_connect('localhost', 'root', '', 'db_atk');
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
 }
